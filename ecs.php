38c45af6e4cbe17f70f0fa49587316aa7a11fa67
Wilmer Arambula <terabytesoftw@gmail.com>
2025-08-18 11:14:01 -0400

    fix(support): Refactor project structure and update dependencies.

--- ecs.php
+++ ecs.php
@@ -5,43 +5,74 @@ declare(strict_types=1);
 use PhpCsFixer\Fixer\ClassNotation\ClassDefinitionFixer;
 use PhpCsFixer\Fixer\ClassNotation\OrderedClassElementsFixer;
 use PhpCsFixer\Fixer\ClassNotation\OrderedTraitsFixer;
+use PhpCsFixer\Fixer\ClassNotation\VisibilityRequiredFixer;
 use PhpCsFixer\Fixer\Import\NoUnusedImportsFixer;
+use PhpCsFixer\Fixer\Import\OrderedImportsFixer;
+use PhpCsFixer\Fixer\StringNotation\SingleQuoteFixer;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
-use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
-return function (ECSConfig $ecsConfig): void {
-    $ecsConfig->paths(
-        [
-            __DIR__ . '/src',
-            __DIR__ . '/tests',
-        ]
-    );
-
-    // this way you add a single rule
-    $ecsConfig->rules(
-        [
-            OrderedClassElementsFixer::class,
-            OrderedTraitsFixer::class,
-            NoUnusedImportsFixer::class,
-        ]
-    );
-
-    // this way you can add sets - group of rules
-    $ecsConfig->sets(
-        [
-            // run and fix, one by one
-            SetList::DOCBLOCK,
-            SetList::NAMESPACES,
-            SetList::COMMENTS,
-            SetList::PSR_12,
-        ]
-    );
-
-    // this way configures a rule
-    $ecsConfig->ruleWithConfiguration(
+return ECSConfig::configure()
+    ->withConfiguredRule(
         ClassDefinitionFixer::class,
         [
             'space_before_parenthesis' => true,
         ],
+    )
+    ->withConfiguredRule(
+        OrderedClassElementsFixer::class,
+        [
+            'order' => [
+                'use_trait',
+                'constant_public',
+                'constant_protected',
+                'constant_private',
+                'case',
+                'property_public',
+                'property_protected',
+                'property_private',
+                'construct',
+                'destruct',
+                'magic',
+                'phpunit',
+                'method_public',
+                'method_protected',
+                'method_private',
+            ],
+            'sort_algorithm' => 'alpha',
+        ],
+    )
+    ->withConfiguredRule(
+        OrderedImportsFixer::class,
+        [
+            'imports_order' => ['class', 'function', 'const'],
+            'sort_algorithm' => 'alpha',
+        ],
+    )
+    ->withConfiguredRule(
+        VisibilityRequiredFixer::class,
+        [
+            'elements' => [],
+        ],
+    )
+    ->withFileExtensions(['php'])
+    ->withPaths(
+        [
+            __DIR__ . '/src',
+            __DIR__ . '/tests',
+        ],
+    )
+    ->withPhpCsFixerSets(perCS20: true)
+    ->withPreparedSets(
+        cleanCode: true,
+        comments: true,
+        docblocks: true,
+        namespaces: true,
+        strict: true,
+    )
+    ->withRules(
+        [
+            NoUnusedImportsFixer::class,
+            OrderedTraitsFixer::class,
+            SingleQuoteFixer::class,
+        ]
     );
-};
--- tests/AssertTest.php
+++ tests/AssertTest.php
@@ -5,68 +5,153 @@ declare(strict_types=1);
 namespace PHPForge\Support\Tests;
 
 use PHPForge\Support\Assert;
+use PHPForge\Support\Tests\Stub\{TestBaseClass, TestClass};
 use PHPUnit\Framework\TestCase;
+use ReflectionException;
 use RuntimeException;
 
 /**
- * @psalm-suppress PropertyNotSetInConstructor
+ * Test suite for {@see Assert} utility methods and reflection helpers.
+ *
+ * Verifies the behavior of assertion and reflection-based utility methods for testing inaccessible properties, parent
+ * properties, and methods, as well as file system operations for test directories.
+ *
+ * These tests ensure correct access and mutation of private/protected members, invocation of inaccessible methods, and
+ * robust file removal logic, including error handling for non-existent directories.
+ *
+ * Test coverage.
+ * - Accessing and asserting values of inaccessible properties and parent properties.
+ * - Ensuring correct exception handling for invalid operations.
+ * - Invoking inaccessible methods and parent methods.
+ * - Removing files from directories and handling missing directories.
+ * - Setting values for inaccessible properties and parent properties.
+ *
+ * @copyright Copyright (C) 2025 Terabytesoftw.
+ * @license https://opensource.org/license/bsd-3-clause BSD 3-Clause License.
  */
 final class AssertTest extends TestCase
 {
-    public function testEqualsWithoutLE(): void
+    public function testEqualsWithoutLEReturnsTrueWhenStringsAreIdenticalWithLineEndings(): void
     {
-        Assert::equalsWithoutLE('foo' . "\r\n" . 'bar', 'foo' . "\r\n" . 'bar');
+        Assert::equalsWithoutLE(
+            "foo\r\nbar",
+            "foo\r\nbar",
+            "Should return 'true' when both strings are identical including line endings.",
+        );
     }
 
-    public function testInaccessibleProperty(): void
+    /**
+     * @throws ReflectionException
+     */
+    public function testInaccessibleParentPropertyReturnsExpectedValue(): void
     {
-        $object = new class () {
-            private string $foo = 'bar';
-        };
-
-        $this->assertSame('bar', Assert::inaccessibleProperty($object, 'foo'));
+        self::assertSame(
+            'valueParent',
+            Assert::inaccessibleParentProperty(
+                new TestClass(),
+                TestBaseClass::class,
+                'propertyParent',
+            ),
+            "Should return the value of the parent property 'propertyParent' when accessed via reflection.",
+        );
     }
 
-    public function testInvokeMethod(): void
+    public function testRemoveFilesFromDirectoryRemovesAllFiles(): void
     {
-        $object = new class () {
-            protected function foo(): string
-            {
-                return 'foo';
-            }
-        };
+        $dir = dirname(__DIR__) . '/runtime';
 
-        $this->assertSame('foo', Assert::invokeMethod($object, 'foo'));
-    }
-
-    public function testSetInaccessibleProperty(): void
-    {
-        $object = new class () {
-            private string $foo = 'bar';
-        };
-
-        Assert::setInaccessibleProperty($object, 'foo', 'baz');
-
-        $this->assertSame('baz', Assert::inaccessibleProperty($object, 'foo'));
-    }
-
-    public function testRemoveFilesFromDirectory(): void
-    {
-        $dir = __DIR__ . '/runtime';
-
-        mkdir($dir);
-        mkdir($dir . '/subdir');
-        touch($dir . '/test.txt');
-        touch($dir . '/subdir/test.txt');
+        mkdir("{$dir}/subdir");
+        touch("{$dir}/test.txt");
+        touch("{$dir}/subdir/test.txt");
 
         Assert::removeFilesFromDirectory($dir);
 
-        $this->assertFileDoesNotExist($dir . '/test.txt');
-
-        rmdir(__DIR__ . '/runtime');
+        $this->assertFileDoesNotExist(
+            "{$dir}/test.txt",
+            "File 'test.txt' should not exist after 'removeFilesFromDirectory' method is called.",
+        );
+        $this->assertFileDoesNotExist(
+            "{$dir}/subdir/test.txt",
+            "File 'subdir/test.txt' should not exist after 'removeFilesFromDirectory' method is called.",
+        );
     }
 
-    public function testRemoveFilesFromDirectoryWithException(): void
+    /**
+     * @throws ReflectionException
+     */
+    public function testReturnInaccessiblePropertyValueWhenPropertyIsPrivate(): void
+    {
+        self::assertSame(
+            'value',
+            Assert::inaccessibleProperty(new TestClass(), 'property'),
+            "Should return the value of the private property 'property' when accessed via reflection.",
+        );
+    }
+
+    /**
+     * @throws ReflectionException
+     */
+    public function testReturnValueWhenInvokingInaccessibleMethod(): void
+    {
+        $this->assertSame(
+            'value',
+            Assert::invokeMethod(new TestClass(), 'inaccessibleMethod'),
+            "Should return 'value' when invoking the inaccessible method 'inaccessibleParentMethod' on 'TestClass' " .
+            'via reflection.',
+        );
+    }
+
+    /**
+     * @throws ReflectionException
+     */
+    public function testReturnValueWhenInvokingInaccessibleParentMethod(): void
+    {
+        $this->assertSame(
+            'valueParent',
+            Assert::invokeParentMethod(
+                new TestClass(),
+                TestBaseClass::class,
+                'inaccessibleParentMethod',
+            ),
+            "Should return 'valueParent' when invoking the inaccessible parent method 'inaccessibleParentMethod' on " .
+            "'TestClass' via reflection.",
+        );
+    }
+
+    /**
+     * @throws ReflectionException
+     */
+    public function testSetInaccessibleParentProperty(): void
+    {
+        $object = new TestClass();
+
+        Assert::setInaccessibleParentProperty($object, TestBaseClass::class, 'propertyParent', 'foo');
+
+        $this->assertSame(
+            'foo',
+            Assert::inaccessibleParentProperty($object, TestBaseClass::class, 'propertyParent'),
+            "Should return 'foo' after setting the parent property 'propertyParent' via " .
+            "'setInaccessibleParentProperty' method.",
+        );
+    }
+
+    /**
+     * @throws ReflectionException
+     */
+    public function testSetInaccessiblePropertySetsValueCorrectly(): void
+    {
+        $object = new TestClass();
+
+        Assert::setInaccessibleProperty($object, 'property', 'foo');
+
+        $this->assertSame(
+            'foo',
+            Assert::inaccessibleProperty($object, 'property'),
+            "Should return 'foo' after setting the private property 'property' via 'setInaccessibleProperty' method.",
+        );
+    }
+
+    public function testThrowRuntimeExceptionWhenRemoveFilesFromDirectoryNonExistingDirectory(): void
     {
         $this->expectException(RuntimeException::class);
         $this->expectExceptionMessage('Unable to open directory: non-existing-directory');
